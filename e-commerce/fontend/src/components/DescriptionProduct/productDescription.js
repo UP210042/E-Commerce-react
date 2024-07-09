4a856dc7b6b72ebf6c10aca00b3dc5e54940a88c
up210368 <113202657+up210368@users.noreply.github.com>
2024-07-08 19:05:02 -0600

    footer y descriptionproduct

---
+++ e-commerce/fontend/src/components/DescriptionProduct/productDescription.js
@@ -0,0 +1,11 @@
+import "./descriptionProduct.css";
+
+const descriptionProduct = (props) => {
+  return (
+    <>
+      <p>lorem epsium</p>
+    </>
+  );
+};
+
+export default descriptionProduct;
---
+++ e-commerce/fontend/src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+const footer = (props) => {
+  return (
+    <>
+      <img alt="Logo empresa"></img>
+      <a>Link empresa</a>
+      <img alt="Redes de la empresa"></img>
+    </>
+  );
+};
+
+export default footer;
